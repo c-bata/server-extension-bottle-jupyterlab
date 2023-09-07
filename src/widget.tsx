0765ca623fef2a28edf8c922b04238dba7363c34
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2023-09-07 22:46:05 +0900

    Make _is_initialized a global state

--- src/widget.tsx
+++ src/widget.tsx
@@ -2,20 +2,20 @@ import { ReactWidget } from '@jupyterlab/ui-components';
 import React, { useState } from 'react';
 import { App } from './components/App';
 import { InitDashboard } from './components/InitDashboard';
-import { requestAPI } from './handler';
+//import { requestAPI } from './handler';
 
 
-const MyComponent = () => {
+const RegisterDashboard = () => {
   //const [loading, setLoading] = useState(false)
   const [isInitialized, setIsInitialized] = useState(false)
 
-  requestAPI<boolean>(`/api/isinitialized`, {
-    method: 'GET',
-  }).then((res) => {
-    setIsInitialized(res);
-  }).catch((err) => {
-    console.log(err)
-  })
+  //requestAPI<boolean>(`/api/register_dashboard_app`, {
+  //  method: 'GET',
+  //}).then((res) => {
+  //  setIsInitialized(res);
+  //}).catch((err) => {
+  //  console.log(err)
+  //})
 
   if (!isInitialized) {
     return (
@@ -35,6 +35,6 @@ export class OptunaDashboardWidget extends ReactWidget {
   }
 
   render(): JSX.Element {
-    return <MyComponent />;
+    return <RegisterDashboard />;
   }
 }
