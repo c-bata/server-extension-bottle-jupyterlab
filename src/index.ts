4b1a30ab9660fad5dda8f19e0b779f74da987213
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2023-09-07 22:13:24 +0900

    Remove unnecessary request

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,10 @@ import { ICommandPalette } from '@jupyterlab/apputils';
 
 import { ILauncher } from '@jupyterlab/launcher';
 
-import { requestAPI } from './handler';
 
+import { MainAreaWidget } from '@jupyterlab/apputils';
 import { reactIcon } from '@jupyterlab/ui-components';
 import { OptunaDashboardWidget } from './widget';
-import { MainAreaWidget } from '@jupyterlab/apputils';
 
 
 /**
@@ -43,30 +42,6 @@ const plugin: JupyterFrontEndPlugin<void> = {
     );
     console.log('ICommandPalette:', palette);
 
-    // Try avoiding awaiting in the activate function because
-    // it will delay the application start up time.
-    requestAPI<any>('hello')
-      .then(data => {
-        console.log(data);
-      })
-      .catch(reason => {
-        console.error(
-          `The jupyterlab_examples_server server extension appears to be missing.\n${reason}`
-        );
-      });
-
-    // POST request
-    const dataToSend = { name: 'George' };
-    requestAPI<any>('init', {
-       body: JSON.stringify(dataToSend),
-       method: 'POST'
-     }).then(reply => {
-      console.log(reply);
-    }).catch(reason => {
-      console.error(
-        `Error on POST /jupyterlab-examples-server/hello ${dataToSend}.\n${reason}`
-      );
-    });
 
     const { commands, shell } = app;
 
